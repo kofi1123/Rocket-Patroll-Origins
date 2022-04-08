87940e08aa604ab05b8c02bb23fadb5806214150
Kofi Quansah <kquansah@ucsc.edu>
2022-04-08 15:24:28 -0700

    movement works

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ let config = {
     scene: [Menu, Play],
 };
 
+let keyF, keyR, keyLEFT, keyRIGHT
+
 let borderUISize = config.height / 15;
 let borderPadding = borderUISize / 3
 let game = new Phaser.Game(config);
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,6 +10,11 @@ class Play extends Phaser.Scene {
     }
 
     create() {
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
         this.bg = this.add.tileSprite(0,0, game.config.width, game.config.height, 'starfield').setOrigin(0, 0);
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00ff00).setOrigin(0,0);
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xffffff).setOrigin(0, 0);
@@ -21,6 +26,15 @@ class Play extends Phaser.Scene {
     }
     update() {
         this.bg.tilePositionX += 4;
-        this.p1Rocke
+        const movementSpeed = 4;
+        if (keyLEFT.isDown) {
+            this.p1Rocket.x -= movementSpeed;
+        }
+        if (keyRIGHT.isDown) {
+            this.p1Rocket.x += movementSpeed;
+        }
+        if (Phaser.Input.Keyboard.JustDown(keyF)) {
+            debugger;
+        }
     }
 }
